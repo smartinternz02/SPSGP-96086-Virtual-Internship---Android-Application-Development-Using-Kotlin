0af6f31458326bbb7a7dc36d26b6b42cfc4f21e7
inikitha <niki09780@gmail.com>
2022-09-29 19:36:10 +0530

    grocery lister

---
+++ app/src/main/java/com/nikitha/grocery/GroceryRepository.kt
@@ -0,0 +1,8 @@
+package com.nikitha.grocery
+
+class GroceryRepository(private val db: GroceryDatabase) {
+    suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
+    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
+
+    fun getAllItems() = db.getGroceryDao().getAllGroceryItems()
+}
